8a04909c466c6d20374ea4efe24bcb1e9ad61f96
Giorgi Adeishvili <adeishvili65@gmail.com>
2023-12-09 16:14:45 +0400

    Done(exercise 13)

---
+++ exercise_13.py
@@ -0,0 +1,15 @@
+# 13. Write a Python program to print the following 'here document'.
+# Sample string :
+# a string that you "don't" have to escape
+# This
+# is a ....... multi-line
+# heredoc string --------> example
+
+# we use triple double quotoes for multiline strings
+
+print("""
+a string that you "don't" have to escape
+This
+is a ....... multi-line
+heredoc string --------> example
+""")
