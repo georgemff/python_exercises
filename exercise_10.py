febbff8c2dc2fb73c349ba1063510ab39f2eecfd
Giorgi Adeishvili <adeishvili65@gmail.com>
2023-12-09 16:02:40 +0400

    fix comments

--- exercise_10.py
+++ exercise_10.py
@@ -1,4 +1,4 @@
-# Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn.
+# 10. Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn.
 # Sample value of n is 5
 # Expected Result : 615
 
--- exercise_8.py
+++ exercise_8.py
@@ -1,4 +1,4 @@
-# Write a Python program to display the first and last colors from the following list.
+# 8. Write a Python program to display the first and last colors from the following list.
 # color_list = ["Red","Green","White" ,"Black"]
 
 color_list = ["Red","Green","White" ,"Black"]
